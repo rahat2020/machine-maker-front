b0a17862d3f07c7ec1ca494af30e66aa34f575fd
rahat2020 <kazimdboktiar2020@gmail.com>
2023-08-28 00:33:45 +0600

    auth fixing

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth"
-// import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 export const authOptions = {
     providers: [
@@ -7,10 +6,6 @@ export const authOptions = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_CLIENT_ID,
-        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        // })
     ],
     pages: {
         signIn: "/login"
